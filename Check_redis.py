af873b8cc973185e998d732e00a745e080f68dc7
Droshow <71423354+Droshow@users.noreply.github.com>
2020-10-18 19:54:23 +0200

    Update Check_redis.py

--- Check_redis.py
+++ Check_redis.py
@@ -4,7 +4,7 @@ import subprocess
 
 
 def main():
-        kenntwort= "3d66a3e822d96f9025cb8a0b1e30e97bc6f39ff74b8358e3153280a9cf84a195123"
+        kenntwort= "3d66a3e822d96f9025cb8a0b1e30"
         pako = subprocess.call(['./redis-cli', '-h', '10.171.139.78', '-p', '6379', '-a', kenntwort],shell=True)
         subprocess.call("set test1 test_werte", shell=True)
         result = subprocess.call("get test1", shell=True)
@@ -25,5 +25,5 @@ print subprocess.check_output(["ping", "-c", "1", "8.8.8.8"])
 
 10.171.139.25
 
-subprocess.call("./redis-cli -h 10.171.139.78 -p 6379 -a 3d66a3e822d96f9025cb8a0b1e30e97bc6f39ff74b8358e3153280a9cf84a195123")
+subprocess.call("call your redis cli ")
 +
